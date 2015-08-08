530d7e43281eef61708fdd50762b7e82f70170bf
zero91 <jianzhang9102@gmail.com>
2015-08-08 13:17:12 +0800

    remove old code, add ThinkPHP framwork

--- code/conf/tenpay/tenpay_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$spname="财付通双接口测试";
-$partner = "1221798901";                      //财付通商户号
-$key = "fe655d8665fe49f5dcaacd2d38ee86d1";    //财付通密钥
-
-$return_url = SITE_URL . "?ebank/tenpayreturn";    //显示支付结果页面,*替换成payReturnUrl.php所在路径
-$notify_url = SITE_URL . "?ebank/tenpaynotify";    //支付完成后的回调处理页面,*替换成payNotifyUrl.php所在路径
-?>
--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
 
-error_reporting(0);
+// 应用入口文件
 
-$mtime = explode(' ', microtime());
-$starttime = $mtime[1] + $mtime[0];
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
-define('IN_SITE', TRUE);
-define('WEB_ROOT', dirname(__FILE__));
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . substr($_SERVER['PHP_SELF'], 0, -10));
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
 
-define('MAGIC_QUOTES_GPC', get_magic_quotes_gpc());
+define('BIND_MODULE','Home');
 
-include WEB_ROOT . '/code/core/boostme.class.php';
+// 定义应用目录
+define('APP_PATH','./Application/');
 
-$boostme = new boostme();
-$boostme->run();
-
-?>
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+// 亲^_^ 后面不需要任何代码了 就是如此简单
